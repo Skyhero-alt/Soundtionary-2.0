a6892b27b3610745259391c2545ad53eeb709a75
Sayed Mohammed Nouman <118057767+Skyhero-alt@users.noreply.github.com>
2022-12-10 14:42:40 +0000

    feat: Added default data theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <Component {...pageProps} data-theme="dark" />;
 }
 
 export default MyApp;
