0d3a542128f63ee107f30eb146bf81363787d08e
Sayed Mohammed Nouman <118057767+Skyhero-alt@users.noreply.github.com>
2022-12-12 09:07:16 +0000

    Initialised socket io on serverside

--- pages/api/server.js
+++ pages/api/server.js
@@ -1,19 +1,14 @@
-import { createServer } from 'http'
-import { Server } from 'ws'
+import { Server } from "Socket.IO";
 
-const httpServer = createServer()
-const wss = new Server({ server: httpServer })
+const SocketHandler = (req, res) => {
+  if (res.socket.server.io) {
+    console.log("Socket is already running");
+  } else {
+    console.log("Socket is initializing");
+    const io = new Server(res.socket.server);
+    res.socket.server.io = io;
+  }
+  res.end();
+};
 
-wss.on('connection', (ws) => {
-  console.log('New web socket connection')
-
-  ws.on('message', (data) => {
-    console.log('Received message:', data)
-
-    ws.send(`Hello, you sent: ${data}`)
-  })
-})
-
-httpServer.listen(3000, () => {
-  console.log('Web socket server listening on port 3000')
-})
+export default SocketHandler;
