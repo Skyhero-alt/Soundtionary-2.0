f751959355a5f67437b981d2fac76438c289a096
Skyhero-alt <skyhero.code@gmail.com>
2022-12-20 14:15:08 +0530

    feat: Connected client and server

--- client/pages/mySocket.js/index.js
+++ client/pages/mySocket.js/index.js
@@ -1,13 +1,21 @@
 import { io } from "socket.io-client";
-
-const socket = io("http://localhost:8000");
+import React from "react";
 
 export default function Server() {
+  // let socket = null;
   return (
     <div>
-      {socket ? console.log("hello ahaha") : null}
-      Lol
-      <button>lol again</button>
+      {/* {socket.connected ? <h1>Connected</h1> : <h2>Not connected</h2>} */}
+      <button
+        onClick={() => {
+          let socket = io("http://localhost:8000");
+          socket.on("connect", (socket) => {
+            console.log("Yahooo");
+          });
+        }}
+      >
+        Lol button
+      </button>
     </div>
   );
 }
